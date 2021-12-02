913cea369838e4cd2e41711aa444c57a97e43438
jucavi <jcarvil@gmail.com>
2021-12-02 21:58:21 +0100

    Add code

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -1,30 +1,63 @@
 import requests
+import random
 
 URL = 'https://pokeapi.co/'
+elemnts = ['fire', 'water', 'grass']
 
 class Pokemon:
-    def __init__(self, name, element, hp, attacks):
+    def __init__(self, name, element, hp, attacks=None):
         self.name = name
         self.element = element
         self.hp = hp
-        self.atacks = attacks
+        self.attacks = attacks or []
         
     def learn(self, attack):
         self.attacks.append(attack)
         
-    def attack(self, other, attack):
-        # relacion elementos y self.element ?
-        if attack.name in self.attacks:
-            other.hp - self.attak.dammage
+    def attack(self,  other, attack):
+        # Check elements ?
+        other.hp -= attack.damage
     
-    def receive_damage(self, other_attack):
-        # relacion elementos y self.element ?
-        self.hp - other_attack.damage
-    
+    def __str__(self):
+        return f'{self.__class__.__name__}({self.name}, {self.element}, {self.hp}, {self.attacks})'
 
 class Attack:
     def __init__(self, name, element, damage):
         self.name = name
         self.element = element
-        self.dmage = damage
+        self.damage = damage
+
+    def __str__(self):
+        return f'{self.__class__.__name__}({self.name}, {self.element}, {self.damage})'
+    
+
+pikachu = Pokemon('Pikachu', 'electric', 180)
+charmander = Pokemon('Charmander', 'fire', 183)
+
+fire_attack = Attack('Flametrower', 'fire', 52)
+electric_attack = Attack('Static', 'electric', 55)
+
+pikachu.learn(electric_attack)
+charmander.learn(fire_attack)
+
+
+pokemons = [pikachu, charmander]
+random.shuffle(pokemons)
+print('[1] ?????')
+print('[2] ?????')
+user = int(input('Pokemon: '))
+user_pokemon = pokemons.pop(user)
+IA_pokemon = pokemons.pop()
+
+time_attack = random.choice((True, False))
+
+while user_pokemon.hp > 0 and IA_pokemon.hp > 0:
+    if time_attack:
+        user_pokemon.attack(IA_pokemon, user_pokemon.attacks[0])
+    else:
+        IA_pokemon.attack(user_pokemon, IA_pokemon.attacks[0])
         
+    time_attack = not time_attack
+    
+print(user_pokemon)
+print(IA_pokemon)
