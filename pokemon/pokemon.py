adb6ab1eab0c6ed8d21306604f3a1e13e618e3b0
Jucavi <jcarvil@gmail.com>
2021-12-04 11:14:03 +0100

    Mess with git

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -1,25 +1,40 @@
 import requests
 import random
+import os
+import time
 
-URL = 'https://pokeapi.co/'
-elemnts = ['fire', 'water', 'grass']
 
 class Pokemon:
-    def __init__(self, name, element, hp, attacks=None):
+    elements = ['fire', 'grass', 'water']
+    
+    def __init__(self, name, element, hp):
         self.name = name
-        self.element = element
+        self.HP = hp
         self.hp = hp
-        self.attacks = attacks or []
+        self.element = element
+        self.attacks = []
+        
         
     def learn(self, attack):
         self.attacks.append(attack)
         
     def attack(self,  other, attack):
         # Check elements ?
-        other.hp -= attack.damage
+        if ((Pokemon.elements.index(other.element) == 0 and Pokemon.elements.index(attack.element) == 2)
+            or (self.elements.index(attack.element) - self.elements.index(other.element) == 1)):
+            damage = attack.damage * 1.5
+        else:
+            damage = attack.damage
+            
+        other.hp -= damage
+        return damage
+            
+    def is_alive(self):
+        return self.hp >= 0
     
-    def __str__(self):
+    def __repr__(self):
         return f'{self.__class__.__name__}({self.name}, {self.element}, {self.hp}, {self.attacks})'
+
 
 class Attack:
     def __init__(self, name, element, damage):
@@ -27,37 +42,94 @@ class Attack:
         self.element = element
         self.damage = damage
 
-    def __str__(self):
+    def __repr__(self):
         return f'{self.__class__.__name__}({self.name}, {self.element}, {self.damage})'
-    
 
-pikachu = Pokemon('Pikachu', 'electric', 180)
-charmander = Pokemon('Charmander', 'fire', 183)
+   
+class Pokemons:
+    def __init__(self, elements, limit=10):
+        self.pokemons = []
+        self.limit = limit
+        self.elements = elements
+        self.URL = 'https://pokeapi.co/api/v2'
 
-fire_attack = Attack('Flametrower', 'fire', 52)
-electric_attack = Attack('Static', 'electric', 55)
-
-pikachu.learn(electric_attack)
-charmander.learn(fire_attack)
-
-
-pokemons = [pikachu, charmander]
-random.shuffle(pokemons)
-print('[1] ?????')
-print('[2] ?????')
-user = int(input('Pokemon: '))
-user_pokemon = pokemons.pop(user)
-IA_pokemon = pokemons.pop()
-
-time_attack = random.choice((True, False))
-
-while user_pokemon.hp > 0 and IA_pokemon.hp > 0:
-    if time_attack:
-        user_pokemon.attack(IA_pokemon, user_pokemon.attacks[0])
-    else:
-        IA_pokemon.attack(user_pokemon, IA_pokemon.attacks[0])
         
-    time_attack = not time_attack
+    def load(self):
+        res = requests.get(f'{self.URL}/pokemon/?limit={self.limit}')
+
+        if res.ok:
+            res = res.json()
+            for result in res['results']:
+                response = requests.get(f'{self.URL}/pokemon/{result["name"]}')
+                if response.ok:
+                    response = response.json()
+                    element = response['types'][0]['type']['name']
+                    if element in self.elements:
+                        hp = response['stats'][0]['base_stat']
+                        self.pokemons.append(Pokemon(result['name'], hp, element))
+        
+        
+def hp_bar(pokemon):
+    full = 25
+    steep = int(pokemon.HP / full)
+    level = '=' * int((pokemon.hp / steep))
+    return f'[{level.ljust(full)}]'
     
-print(user_pokemon)
-print(IA_pokemon)
+    
+os.system('clear')
+
+waterdragon = Pokemon('Waterdragon', 'grass', 183)
+charmander = Pokemon('Charmander', 'fire', 180)
+
+flamethrower = Attack("Flamethrower", 'fire', 25)
+water_gun = Attack("Tackle", 'water', 20)
+razor_leaf = Attack("Razor leaf", 'grass', 20)
+
+waterdragon.learn(water_gun)
+charmander.learn(flamethrower)
+
+while True:
+    pokemons = [waterdragon, charmander]
+    random.shuffle(pokemons)
+    print('[1] ?????')
+    print('[2] ?????')
+    user = int(input('Pokemon: '))
+    user_pokemon = pokemons.pop(user - 1)
+    IA_pokemon = pokemons.pop()
+
+    time_attack = random.choice((True, False))
+
+    while user_pokemon.is_alive() and IA_pokemon.is_alive():
+        os.system('clear')
+        print(f'User: {user_pokemon.name} HP: {hp_bar(user_pokemon)}     IA: {IA_pokemon.name} HP: {hp_bar(IA_pokemon)}')
+        if time_attack:
+            for i, attack in enumerate(user_pokemon.attacks, start=1):
+                print(f'[{1}.] {attack.name}')
+            index = int(input('Choose: '))
+            attack = user_pokemon.attacks[index - 1]
+            print(f'{user_pokemon.name} attack with {attack.name}')
+            damage = user_pokemon.attack(IA_pokemon, attack)
+            print(f'{IA_pokemon.name} recive {damage} damage points')
+        else:
+            attack = random.choice(IA_pokemon.attacks)
+            print(f'{IA_pokemon.name} attack with {attack.name}')
+            damage = IA_pokemon.attack(user_pokemon, attack)
+            print(f'{user_pokemon.name} recive {damage} damage points')
+            time.sleep(0.5)
+
+        time_attack = not time_attack
+        time.sleep(1.3)
+
+                
+    winner = IA_pokemon if IA_pokemon.is_alive() else user_pokemon
+    print(f'{winner.name} win!')
+    
+    repeat = input('Another Battle (y/n)? :')
+    if repeat.lower() == 'n':
+        break
+
+# pokemons = Pokemons(elemnts, 150)
+# pokemons.load()
+# print(pokemons.pokemons)
+# for pok in filter(lambda pokemon: pokemon.element == 'fire', pokemons.pokemons):
+#     print(pok)
