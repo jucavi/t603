5de629710622a1c89e61173fefa9b94f755f5987
Jucavi <jcarvil@gmail.com>
2021-12-04 12:22:02 +0100

    Add Screen and Pokemon methods health, HPts, rename attack method

--- pokemon/pokemon.py
+++ pokemon/pokemon.py
@@ -5,35 +5,43 @@ import time
 
 
 class Pokemon:
-    elements = ['fire', 'grass', 'water']
+    _weakness = {'fire': ['water'], 'grass': ['fire'], 'water': ['grass']}
     
     def __init__(self, name, element, hp):
         self.name = name
-        self.HP = hp
+        self._HP = hp
         self.hp = hp
         self.element = element
         self.attacks = []
-        
-        
+    
+    def health(self):
+        return self.hp
+    
+    def HealthPoints(self):
+        return self._HP
+    
     def learn(self, attack):
         self.attacks.append(attack)
         
-    def attack(self,  other, attack):
-        # Check elements ?
-        if ((Pokemon.elements.index(other.element) == 0 and Pokemon.elements.index(attack.element) == 2)
-            or (self.elements.index(attack.element) - self.elements.index(other.element) == 1)):
-            damage = attack.damage * 1.5
-        else:
-            damage = attack.damage
-            
-        other.hp -= damage
-        return damage
+    def charge(self,  other, attack):
+        boost = 1.5 if other._is_weak(attack) else 1      
+        other.hp -= attack.damage * boost
             
     def is_alive(self):
-        return self.hp >= 0
+        return self.health() >= 0
+    
+    def _is_weak(self, attack):
+        return attack.element in Pokemon._weakness[self.element]
+    
+    def recover(self, heal=None):
+        if heal:
+            self.hp += heal
+            
+        if self.health() > self._HP or not heal:
+            self.hp = self._HP
     
     def __repr__(self):
-        return f'{self.__class__.__name__}({self.name}, {self.element}, {self.hp}, {self.attacks})'
+        return f'{self.__class__.__name__}({self.name}, {self.element}, {self.health()}, {self.attacks})'
 
 
 class Attack:
@@ -47,33 +55,28 @@ class Attack:
 
    
 class Pokemons:
-    def __init__(self, elements, limit=10):
+    def __init__(self, limit=10):
         self.pokemons = []
         self.limit = limit
-        self.elements = elements
         self.URL = 'https://pokeapi.co/api/v2'
 
-        
     def load(self):
         res = requests.get(f'{self.URL}/pokemon/?limit={self.limit}')
 
         if res.ok:
             res = res.json()
-            for result in res['results']:
-                response = requests.get(f'{self.URL}/pokemon/{result["name"]}')
-                if response.ok:
-                    response = response.json()
-                    element = response['types'][0]['type']['name']
-                    if element in self.elements:
-                        hp = response['stats'][0]['base_stat']
-                        self.pokemons.append(Pokemon(result['name'], hp, element))
+            return res['results']
+        return []
         
+class Screen:
+    def __init__(self):
+        pass
+    
+    def health_bar(pokemon, size=25):
+        steep = int(pokemon.HealthPoints() / size)
+        amount = '=' * int((pokemon.health() / steep))
         
-def hp_bar(pokemon):
-    full = 25
-    steep = int(pokemon.HP / full)
-    level = '=' * int((pokemon.hp / steep))
-    return f'[{level.ljust(full)}]'
+        return f'[{amount.ljust(size)}]'
     
     
 os.system('clear')
@@ -101,26 +104,27 @@ while True:
 
     while user_pokemon.is_alive() and IA_pokemon.is_alive():
         os.system('clear')
-        print(f'User: {user_pokemon.name} HP: {hp_bar(user_pokemon)}     IA: {IA_pokemon.name} HP: {hp_bar(IA_pokemon)}')
+        print(f'User: {user_pokemon.name} HP: {Screen.health_bar(user_pokemon)}     IA: {IA_pokemon.name} HP: {Screen.health_bar(IA_pokemon)}')
         if time_attack:
             for i, attack in enumerate(user_pokemon.attacks, start=1):
                 print(f'[{1}.] {attack.name}')
             index = int(input('Choose: '))
             attack = user_pokemon.attacks[index - 1]
             print(f'{user_pokemon.name} attack with {attack.name}')
-            damage = user_pokemon.attack(IA_pokemon, attack)
+            damage = user_pokemon.charge(IA_pokemon, attack)
             print(f'{IA_pokemon.name} recive {damage} damage points')
         else:
             attack = random.choice(IA_pokemon.attacks)
             print(f'{IA_pokemon.name} attack with {attack.name}')
-            damage = IA_pokemon.attack(user_pokemon, attack)
+            damage = IA_pokemon.charge(user_pokemon, attack)
             print(f'{user_pokemon.name} recive {damage} damage points')
             time.sleep(0.5)
 
         time_attack = not time_attack
         time.sleep(1.3)
 
-                
+    os.system('clear')
+    print(f'User: {user_pokemon.name} HP: {Screen.health_bar(user_pokemon)}     IA: {IA_pokemon.name} HP: {Screen.health_bar(IA_pokemon)}')        
     winner = IA_pokemon if IA_pokemon.is_alive() else user_pokemon
     print(f'{winner.name} win!')
     
@@ -133,3 +137,12 @@ while True:
 # print(pokemons.pokemons)
 # for pok in filter(lambda pokemon: pokemon.element == 'fire', pokemons.pokemons):
 #     print(pok)
+
+
+# response = requests.get(f'{self.URL}/pokemon/{result["name"]}')
+# if response.ok:
+#     response = response.json()
+#     element = response['types'][0]['type']['name']
+#     if element in self.elements:
+#         hp = response['stats'][0]['base_stat']
+#         self.pokemons.append(Pokemon(result['name'], hp, element))
