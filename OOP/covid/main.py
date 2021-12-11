bff4633a63572ef17efa672f4173a4ab89cd131d
jucavi <jcarvil@gmail.com>
2021-12-11 10:50:25 +0100

    Statistic object

---
+++ OOP/covid/main.py
@@ -0,0 +1,51 @@
+from std import Std
+import requests
+import json
+import os
+import matplotlib.pyplot as plt
+
+url = 'https://datos.comunidad.madrid/catalogo/dataset/b3d55e40-8263-4c0b-827d-2bb23b5e7bab/resource/907a2df0-2334-4ca7-aed6-0fa199c893ad/download/covid19_tia_zonas_basicas_salud_s.json'
+filename = 'covid.json'
+dirname = os.path.dirname(__file__)
+filepath = os.path.join(dirname, filename)
+print(dirname)
+
+if not os.path.isfile(filepath):
+    res = requests.get(url).json()
+    with open(filepath, 'w') as file:
+        json.dump(res, file, indent=4)['data'] 
+        
+def get_data(filename=None):
+    if filename:
+        filepath = filename
+    try:
+        with open(filepath) as file:
+            return json.load(file)
+    except Exception:
+        return []
+
+def get_by_date(data):
+    result = {}
+    for zone in data:
+        sdate = zone['fecha_informe'].split(' ')[0]
+        if result.get(sdate):
+            result[sdate].append(zone['casos_confirmados_totales'])
+        else:
+            result[sdate] = [zone['casos_confirmados_totales']]
+    return result
+
+data = get_data(filename)
+confirmed_per_week = [sum(confirmed) for confirmed in get_by_date(data).values()]
+confirmed_per_week.reverse()
+weeks = list(range(len(confirmed_per_week)))
+
+std = Std(weeks, confirmed_per_week)
+predicts = [std.predict(week) for week in weeks]
+
+plt.legend(loc='upper left')
+plt.xlabel('Weeks')
+plt.ylabel('Acumulated Cases')
+plt.plot(weeks, confirmed_per_week, '-b')
+plt.plot(weeks, predicts, '-r')
+plt.show()
+
--- OOP/covid/std.py
+++ OOP/covid/std.py
@@ -1,7 +1,63 @@
-class std:
+class Std:
     def __init__(self, x, y):
         self.x = x
         self.y = y
-        self.n = len(x)
+        
+    @property
+    def n(self):
+        return len(self.x)
+    
+    @property
+    def x_mean(self):
+        return sum(self.x) / self.n
+    
+    @property
+    def y_mean(self):
+        return sum(self.y) / self.n
+    
+    @property
+    def x_variance(self):
+        return sum((xi - self.x_mean)**2 for xi in self.x) / self.n
+
+    @property
+    def y_variance(self):
+        return sum((yi - self.y_mean)**2 for yi in self.y) / self.n
+    
+    @property
+    def covariance(self):
+        return sum(map(lambda xi, yi: (xi - self.x_mean) * (yi - self.y_mean), self.x, self.y)) / self.n
+    
+    @property
+    def gradient(self):
+        x_sum = sum(self.x)
+        y_sum = sum(self.y)
+        numerator = self.n * sum(x * y for x, y in zip(self.x, self.y)) - (x_sum * y_sum)
+        denominator = (self.n * sum(x**2 for x in self.x)) - x_sum**2
+        return numerator / denominator
+    
+    @property
+    def interception(self):
+        return self.y_mean - self.gradient * self.x_mean
+    
+    @property
+    def r(self):
+        return  self.covariance / ((self.x_variance ** 0.5) * (self.y_variance ** 0.5))
+    
+    def predict(self, x):
+        return self.gradient * x + self.interception
         
     
+    def __str__(self):
+        return f'x: {self.x}\ny: {self.y}\nn: {self.n}'
+
+
+# x = [1,2,3]
+# y = [10,20,27]
+
+# sample = Std(x, y)
+
+# print(sample)
+# # print(sample.covariance)
+# # print(sample.r)
+# print(sample.gradient)
+# print(sample.interception)
