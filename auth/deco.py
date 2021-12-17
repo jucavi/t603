da7c395e23a26f343bdb6d55901d1b41fae67157
Jucavi <jcarvil@gmail.com>
2021-12-17 18:19:10 +0100

    Add login control

--- auth/deco.py
+++ auth/deco.py
@@ -2,57 +2,45 @@ from functools import wraps
 import datetime
 import os
 
-def upper(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        return func(*args, **kwargs).upper()
-    return wrapper
+# def upper(func):
+#     @wraps(func)
+#     def wrapper(*args, **kwargs):
+#         return func(*args, **kwargs).upper()
+#     return wrapper
 
-def em(func):
-    @wraps(func)
-    def wrapper(*args, **kwargs):
-        return f'{func(*args, **kwargs)}!!!'
-    return wrapper
+# def em(func):
+#     @wraps(func)
+#     def wrapper(*args, **kwargs):
+#         return f'{func(*args, **kwargs)}!!!'
+#     return wrapper
 
-def deco_with_args(arg1):
-    def decorator(func):
-        print(f'Passed {arg1=}')
-        @wraps(func)
-        def wrapper(*args, **kwargs):
-            wrap = '*' * arg1
-            return f'{wrap}\n{func(*args, **kwargs)}\n{wrap}'
-        return wrapper
-    return decorator
+# def deco_with_args(arg1):
+#     def decorator(func):
+#         print(f'Passed {arg1=}')
+#         @wraps(func)
+#         def wrapper(*args, **kwargs):
+#             wrap = '*' * arg1
+#             return f'{wrap}\n{func(*args, **kwargs)}\n{wrap}'
+#         return wrapper
+#     return decorator
 
-def logger(func):
-    pass
+# def logger(func):
+#     pass
 
-@deco_with_args(28)
-@upper
-@em
-def greeting(name):
-    return f'Hola {name}'
-
-###########################################
+# @deco_with_args(28)
+# @upper
+# @em
+# def greeting(name):
+#     return f'Hola {name}'
 
 def logger(func):
+    @wraps(func)
     def wrapper(*args, **kwargs):
+        result = func(*args, **kwargs)
+        name = result['username']
         pathfile = os.path.join(os.path.dirname(__file__), 'call.log')
         with open(pathfile, 'a+') as file:
-            file.write(f'{datetime.datetime.now()}| {func.__name__}\n')
-        return func(*args, **kwargs)
+            file.write(f'{datetime.datetime.now()}| User {name} {func.__name__}\n')
+        return result
     return wrapper
 
-@logger
-def greeting(name):
-    return f'Hello {name}'
-
-@logger
-def regards(name):
-    return f'Hi! {name}'
-
-f_list = [regards, greeting, regards, regards] * 6
-name = 'Paul'
-
-for f in f_list:
-    f(name)
--- auth/main.py
+++ auth/main.py
@@ -1,21 +1,77 @@
-users = {'user': []}
+import os
+import json
+import getpass
+import base64
+from time import sleep
+from deco import logger
 
+CWD = os.path.dirname(__file__)
+db_name = 'app_db.json'
+
+@logger
 def signup():
-    pass
+    username = input('User Name: ')
+    while True:
+        password = getpass.getpass('Password: ').encode()
+        password = base64.b64encode(password).decode()
+        conf_pass = getpass.getpass('Confirm password: ').encode()
+        conf_pass = base64.b64encode(conf_pass).decode()
+        if password == conf_pass:
+            break
+    user = {
+        'username': username,
+        'password': password
+    }
+    users.append(user)
+    print(f'User {username} successfully created.')
+    sleep(1)
+    return user
 
+def find_user(name):
+    return list(filter(lambda user: user['username'] == name, users))[0]
+
+@logger
 def login():
-    pass
+    name = input('User name: ')
+    password = getpass.getpass('Password: ')
+    user = find_user(name)
+
+    if user:
+        user_password = base64.b64decode((user['password'].encode())).decode()
+        if user_password == password:
+            print(f'User {name} log in')
+            sleep(1)
+            return user
+
+def load_db(name, path='.'):
+    try:
+        with open(os.path.join(path, name)) as f:
+            data = json.load(f)
+        return data
+    except:
+        print(f'Unable to read {name!r}')
+        return {'data': {}}
+
+def write_db(data, name, path='.'):
+    with open(os.path.join(path, name), 'w') as f:
+            json.dump(data, f, indent=4, ensure_ascii=False)
+
+
+data = load_db('app_db.json', path=CWD)
+users = data['data'].setdefault('users', [])
 
 while True:
+    os.system('clear')
     print('[1] Signup')
     print('[2] Login')
     print('[Q] Exit')
     option = input('> ')
 
     if option.lower() == 'q':
+        write_db(data, db_name, path=CWD)
         break
 
     if option == '1':
         signup()
     elif  option == '2':
-        pass
+        login()
